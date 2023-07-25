524990820822859cb8933ad5f465c7399ead2ef5
pwsmith4 <97992667+pwsmith4@users.noreply.github.com>
2023-07-25 16:21:48 -0700

    Add project files.

---
+++ Practice/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Practice;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Practice/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Practice;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
